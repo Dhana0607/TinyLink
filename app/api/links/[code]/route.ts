e1f3a6ac30825d96a20d1e9cae4f288ba0c2aed7
Dhana0607 <dhanalakshmi.r.2019.cse@rajalakshmi.edu.in>
2025-11-23 22:38:44 +0530

    Fix prisma for deployment

--- app/api/links/[code]/route.ts
+++ app/api/links/[code]/route.ts
@@ -1,6 +1,6 @@
 // app/api/links/[code]/route.ts
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import prisma from "../../../../lib/prisma";
 
 export async function GET(_req: NextRequest, { params }: { params: Promise<{ code: string }> }) {
   const { code } = await params; // <<-- must await
